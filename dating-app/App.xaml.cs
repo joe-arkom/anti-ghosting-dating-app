583c31d1d6f80fccb0b77bfbeecc40a31ee912a6
joe-arkom <joseph.allen@arkom.co.uk>
2023-10-21 18:05:36 +0100

    Add project files.

---
+++ dating-app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace dating_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
